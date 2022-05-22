049452496b49e3282b685a7e4423c7a015ed8eee
parth-chovatiya <parthchovatiya3@gmail.com>
2022-05-22 21:39:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Website.
+
+**If you enjoy this project, please consider [supporting me](https://www.paypal.me/parthchovatiya) to continue developing and maintaining it.**
---
+++ db/db.js
@@ -0,0 +1,339 @@
+export default {
+  bio: {
+    about: {
+      text: [
+        "Hi &#128075;",
+        "I'm Parth Somawat. I’m currently working as a Software Engineer at Biofourmis India specialized in JavaScript Applications. I did my undergrad in CS from NIT Warangal.",
+        "I'm a developer, geek and curious human besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
+        "I love to read, listen to music, and travel.",
+      ],
+    },
+    contact: {
+      text: [
+        "If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: parthchovatiya3@gmail.com",
+      ],
+    },
+  },
+  skills: [
+    {
+      title: "DSA",
+      skillName: "DS & Algorithms",
+      color: "1",
+      percentage: "80",
+    },
+    {
+      title: "Languages",
+      skillName: "C, C++",
+      color: "1",
+      percentage: "75",
+    },
+    {
+      title: "Languages",
+      skillName: "JavaScipt, TypeScript",
+      color: "1",
+      percentage: "70",
+    },
+    {
+      title: "Frameworks/Libraries",
+      skillName: "Reactjs, Nextjs",
+      color: "6",
+      percentage: "75",
+    },
+    {
+      title: "Backend",
+      skillName: "Nodejs, MongoDB",
+      color: "2",
+      percentage: "70",
+    },
+    {
+      title: "Clouds",
+      skillName: "AWS(EC2, S3), Heroku, Netlify",
+      color: "3",
+      percentage: "30",
+    },
+    {
+      title: "Design",
+      skillName: "HTML, Bootstrap, SCSS",
+      color: "4",
+      percentage: "85",
+    },
+    {
+      title: "Tools",
+      skillName: "Postman, GitHub",
+      color: "3",
+      percentage: "80",
+    },
+  ],
+  projects: {
+    web: [
+      {
+        projectName: "Covid 19 Bad Vacancy Info",
+        image: "images/covid.png",
+        summary:
+          "Developed a full-stack web application to find an empty bed for patients across any city.",
+        preview: "https://github.com/parth-chovatiya/Covid-19-Bad-Vacancy-info",
+        techStack: [
+          "PHP",
+          "MySql",
+          "Bootstrap",
+          "JavaScript",
+          // "Heroku",
+        ],
+      },
+      {
+        projectName: "BookBuzz",
+        image: "images/bookbuzz.png",
+        summary:
+          "Full-stack web application, where users can write notes, write a review of the book, and give a rating to books.",
+        preview: "https://github.com/krishnan-tech/bookbuzz",
+        techStack: [
+          "Nextjs",
+          "Nodejs",
+          "Expressjs",
+          "TypeScript",
+          "MongoDB",
+          "ChakraUI",
+          // "Netlify",
+        ],
+      },
+      {
+        projectName: "Training & Placement Management",
+        image: "images/tpc.png",
+        summary:
+          "Full-Stack web application that will Manage all necessary information that is required by students and companies for campus hiring.",
+        preview:
+          "https://github.com/parth-chovatiya/training-placement-management",
+        techStack: [
+          "Reactjs",
+          "Nodejs",
+          "Expressjs",
+          "JavaScript",
+          "MongoDB",
+          "Bootstrap",
+        ],
+      },
+      {
+        projectName: "To Do Web Application",
+        image: "images/to_do.png",
+        summary:
+          "To-Do Application, where users can write their tasks, edit and delete tasks.",
+        preview: "https://github.com/parth-chovatiya/to-do-webapplication",
+        techStack: ["Django", "Python", "SQlite", "Bootstrap"],
+      },
+      {
+        projectName: "Weather App",
+        image: "images/weather.png",
+        summary:
+          "Weather Application where users can see the weather of any city.",
+        preview: "https://github.com/parth-chovatiya/weather-app",
+        techStack: ["Django", "Python", "SQlite", "Bootstrap"],
+      },
+    ],
+    software: [
+      {
+        projectName: "Number Guessing Game",
+        image: "images/number_guessing_game.png",
+        summary:
+          "Number Guessing Game, where the system generates a random number and if the user predicts a number then they will win, otherwise they can try again.",
+        preview: "https://github.com/parth-chovatiya/Number_guessing_game",
+        techStack: ["Tkinter", "Python"],
+      },
+      // {
+      //   projectName: "WhatsApp-Bot",
+      //   image: "images/whatsappbot.jpg",
+      //   summary:
+      //     "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+      //   preview: "https://github.com/parth_chovatiya/WhatsApp-Bot",
+      //   techStack: ["Selenium", "Chrome Webdriver", "Python"],
+      // },
+      // {
+      //   projectName: "Bill Generator",
+      //   image: "images/billgenerator.png",
+      //   summary:
+      //     "GUI to transfer data to excel sheets and generate bills on the local shops.",
+      //   preview: "https://github.com/parth_chovatiya/Bill-Generator",
+      //   techStack: ["Tkinter", "Openxlpy", "Python"],
+      // },
+    ],
+    android: [
+      // {
+      //   projectName: "NITW-CSE",
+      //   image: "images/nitwcse.jpg",
+      //   summary:
+      //     "The Application display details of Department courses, reference books, research, publication and faculty profile.",
+      //   preview: "https://github.com/parth_chovatiya/NITW-CSE",
+      //   techStack: ["JAVA", "XML", "Android"],
+      // },
+      // {
+      //   projectName: "CareerHigh-App",
+      //   image: "images/carrerhigh.png",
+      //   summary:
+      //     "The Application display the webpages of website careerhigh.in in android devices.",
+      //   preview: "https://github.com/parth_chovatiya/CareerHigh-Android",
+      //   techStack: ["JAVA", "XML", "Android"],
+      // },
+    ],
+    freelance: [
+      // {
+      //   projectName: "SnylloAir.com",
+      //   image: "images/snylloair.png",
+      //   summary:
+      //     "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+      //   preview: "https://www.snylloair.com/",
+      //   techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+      // },
+      // {
+      //   projectName: "Delivery+",
+      //   image: "images/AM-Logo-.png",
+      //   summary: "Android Application to display website in android devices.",
+      //   preview:
+      //     "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+      //   techStack: ["Android", "JAVA", "Play Store"],
+      // },
+    ],
+  },
+  experience: [
+    // {
+    // 	title: "Biofourmis India Pvt. Ltd.",
+    // 	duration: "April 2022 - Present",
+    // 	subtitle: "Software Engineer",
+    // 	details: [
+    // 		"Remote Patient Monitoring System"
+    // 	],
+    // 	tags: [
+    // 		"JavaScript",
+    // 		"Angular",
+    // 		"React",
+    // 		"Bootstrap",
+    // 		"Nodejs",
+    // 		"Jenkins",
+    // 	],
+    // 	icon: "heartbeat",
+    // },
+    // {
+    // 	title: "Novopay Solutions Pvt. Ltd.",
+    // 	duration: "June 2020 - April 2022",
+    // 	subtitle: "Software Engineer",
+    // 	details: [
+    // 		"Supporting both frontend & backend teams for AEPS, DMT transaction, CMS Service, gateways.",
+    // 		"Working on biometric eKYCs, CDM card and onboarding retailers flows.",
+    // 	],
+    // 	tags: [
+    // 		"JavaScript",
+    // 		"Angular",
+    // 		"React",
+    // 		"Bootstrap",
+    // 		"Nodejs",
+    // 		"Jenkins",
+    // 	],
+    // 	icon: "qrcode",
+    // },
+    // {
+    // 	title: "ThinkPedia LLP",
+    // 	duration: "May 2019 - June 2019",
+    // 	subtitle: "SDE Intern",
+    // 	details: [
+    // 		"Worked as a full stack developer to support tech team.",
+    // 		"Developed a customer Web Application from scratch for social media management.",
+    // 	],
+    // 	tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
+    // 	icon: "group",
+    // },
+  ],
+	achievements: [
+		{
+			title: "grtjkgnkjf"
+		}
+	],
+  education: [
+    {
+      title: "Bachelors in Computer Engineering",
+      duration: "",
+      subtitle: "Birla Vishvakarma Mahavidyalaya Engineering College, Anand",
+      details: [
+        "Secured 1st rank in Code Crunch - CSI BVM College Coding Competition organized by BVM CSI.",
+        "Active Competitive Programmer with CodeChef Rating 1820.",
+      ],
+      tags: [
+        "Data Structures & Algorithms",
+        "Operating Systems",
+        "Information Security",
+        "Database Management System",
+        "Computer Networks",
+      ],
+      icon: "graduation-cap",
+    },
+    {
+      title: "Class 11-12th in Science and Mathematics",
+      duration: "",
+      subtitle: "P P Savani Vidhya Bhavan, Surat",
+      details: [
+        "Secured 1045 engineering rank over 40,000+ students.",
+        "Secured 99.08 percentile and 102.75 marks in Gujcet-2019.",
+        "Secured 86.66 percentage in HSC Examination.",
+      ],
+      tags: ["Physics", "Chemistry", "Mathematics"],
+      icon: "book",
+    },
+  ],
+  footer: [
+    {
+      label: "Dev Profiles",
+      data: [
+        {
+          text: "Stackoverflow",
+          link: "https://stackoverflow.com/users/8461233/parth_chovatiya",
+        },
+        {
+          text: "GitHub",
+          link: "https://github.com/parth-chovatiya/",
+        },
+        {
+          text: "LeetCode",
+          link: "https://leetcode.com/parth_chovatiya/",
+        },
+      ],
+    },
+    {
+      label: "Resources",
+      data: [
+        {
+          text: "Enable Dark/Light Mode",
+          func: "enableDarkMode()",
+        },
+        {
+          text: "Print this page",
+          func: "window.print()",
+        },
+        {
+          text: "Clone this page",
+          link: "https://github.com/parth_chovatiya/parth_chovatiya.github.io",
+        },
+      ],
+    },
+    {
+      label: "Social Profiles",
+      data: [
+        {
+          text: "Linkedin",
+          link: "https://www.linkedin.com/in/parth-chovatiya/",
+        },
+        {
+          text: "Twitter",
+          link: "https://twitter.com/ParthChovatiya0",
+        },
+        {
+          text: "Buy me a coffee",
+          link: "https://www.buymeacoffee.com/parthchovatiya",
+        },
+      ],
+    },
+    {
+      label: "copyright-text",
+      data: [
+        "Made with &hearts; by Parth Chovatiya.",
+        "&copy; No Copyrights. Feel free to use this template.",
+      ],
+    },
+  ],
+};
